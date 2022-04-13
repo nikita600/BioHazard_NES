0c4e09b16c69ce3c111cbfd8fa3df0b36f08e9f6
nikita600 <nikitak600@gmail.com>
2022-04-13 23:52:18 +0300

    BANK_12 compiled.

---
+++ src/music/bank_12_apu.asm
@@ -0,0 +1,89 @@
+byte_12858A:
+	.BYTE	2,  2,	2,  8,	8,  2,	2,  8,	8,  8,	8,  2,	2,  2,	2,  2
+	.BYTE	2,  2,	8,  8,	8,  8,	2,  2,	2,  2,	2,  2,	2,  2,	8,  2
+	.BYTE	8,  2,	2,  2,	2,  8,	2,  2,	2,  8,	8,  2,	2,  2,	8,  2
+	.BYTE	2,  2,	2,  2,	8,  2,	2,  2,	2,  2,	2,  2,	8,  2,	2,  2
+	.BYTE	2,  2,	8,  8,	2,  2,	1,  2
+
+byte_1285D2:
+	.BYTE	4,  4,	4, $C, $C,  4,	4, $C, $C, $C, $C,  4,	4,  4,	4,  4
+	.BYTE	4,  4, $C, $C, $C, $C,	4,  4,	4,  4,	4,  4,	4,  4, $C,  4
+	.BYTE  $C,  4,	4,  4,	4, $C,	4,  4,	4, $C, $C,  4,	4,  4, $C,  4
+	.BYTE	4,  4,	4,  4, $C,  4,	4,  4,	4,  4,	4,  4, $C,  4,	4,  4
+	.BYTE	4,  4, $C, $C,	4,  4,	0,  4
+
+apu_pulse_timer1_12861A:
+	.BYTE	$9F, $BA, $F4, $F1 
+	.BYTE	$81, $99, $8D, $A
+	.BYTE	$88, $93, $2E, 0
+	.BYTE	$1F, $7A, $3A, $96
+	.BYTE	$4F, $94, $4C, $6C
+	.BYTE	$DF, $B3, $5A, $20
+	.BYTE	$49, $93, $1B, $30
+	.BYTE	$8F, $94, $39, $6C
+	.BYTE	$DF, $B3, $26, $17
+	.BYTE	$DF, $78, $49, $94
+	.BYTE	$4F, $94, $4C, $6C
+	.BYTE	$9F, $A9, 3, $A1
+	.BYTE	$5F, $CA, $9C, $98
+	.BYTE	$DF, $FA, $14, $D1
+	.BYTE	$9F, $AA, 4, $A2
+	.BYTE	$9F, $FC, $14, $E9
+	.BYTE	$DF, $9A, $FB, 9
+	.BYTE	$9F, $F3, $4F, $A1
+	.BYTE	$80, $E, $1E, $11
+	.BYTE	$9F, $1A, $37, $1F
+	.BYTE	$F, $B6, $F, $10
+	.BYTE	$9D, $68, 7, $15
+	.BYTE	$4F, $B4, $33, $F8
+	.BYTE	$DF, $E1, $4E, $A9
+	.BYTE	$4F, $F2, $1F, 8
+	.BYTE	$8F, $F9, $FB, $F9
+	.BYTE	$9F, $AA, 4, $A1
+	.BYTE	$4F, $B2, $1A, $C0
+	.BYTE	$4F, $BB, $33, $E9
+	.BYTE	$CF, $B3, $5A, $90
+	.BYTE	$DF, $E4, $42, $1A
+	.BYTE	$56, $A9, $6A, $B0
+	.BYTE	0,	0, 8, 0
+	.BYTE	$81, $99, $8D, $A
+	.BYTE	$8D, $CA, $EA, $D0
+	.BYTE	$AB, $C5, $51, $D2
+	.BYTE	$88, $84, $5F, $30
+	.BYTE	$8F, $A1, 9, $F0
+	.BYTE	$88, $93, $2E, 0
+	.BYTE	$4B, $6F, $FB, $D0
+	.BYTE	$FF, $AF, 2, $E3
+	.BYTE	$DF, $E4, $42, $1A
+	.BYTE	$80, $E, $1E, $11
+	.BYTE	$4F, $B4, $33, $F8
+	.BYTE	$DF, $E1, $4E, $A9
+	.BYTE	$4B, $8C, $CD, $30
+	.BYTE	$B, $C4, $8D, $30
+	.BYTE	$4F, $C4, $3F, 8
+	.BYTE	$4C, $9A, $5C, $B0
+	.BYTE	$4F, $9A, $2E, 8
+	.BYTE	$45, $A9, 8, 9
+	.BYTE	$4A, $9A, $D2, $D0
+	.BYTE	$D, 0, $8E, $D0
+	.BYTE	$CC, $8C, $7E, $A9
+	.BYTE	$8C, $D2, $3C, 8
+	.BYTE	$49, $BA, $7E, $21
+	.BYTE	$4C, 0, $6C, $F0
+	.BYTE	$4C, 0, $4F, $F0
+	.BYTE	$8D, $AA, $A6, $F1
+	.BYTE	$1A, $89, $13, 4
+	.BYTE	0,	0, 8, 0
+	.BYTE	$81, $99, $8D, $A
+	.BYTE	$8D, $CA, $EA, $D0
+	.BYTE	$AB, $C5, $51, $D2
+
+stru_12871A:
+	.BYTE	$88, $84, $5F, $30 
+	.BYTE	$88, $93, $2E, 0
+	.BYTE	$8F, $A1, 9, $F0
+	.BYTE	$B, $83, $E, $40
+	.BYTE	$49, $F2, $C4, 8
+	.BYTE	$9F, $BA, $CC, $20
+	.BYTE	$8D, $AA, $A6, $F1
+	.BYTE	$4F, $C3, $2D, 8
